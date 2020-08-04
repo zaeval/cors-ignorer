f7e4b4e9be47dd254de00db5c7c9ad2ae23a8b41
zaeval <zaeval@kookmin.ac.kr>
2020-08-05 01:38:50 +0900

    cors

--- routes/index.js
+++ routes/index.js
@@ -16,17 +16,17 @@ router.get('/*', async function (req, res, next) {
     try {
         headers["origin"] = new URL(url).origin;
         headers["host"] = new URL(url).host;
-        headers["user-agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36";
 
         const response = await fetch(url, {headers: headers, agent: httpsAgent, method: 'GET'});
         const responseHeaders = JSON.parse(JSON.stringify(response.headers.raw()));
         const responseData = await response.text();
-        for (key in responseHeaders) {
-            res.set(key, responseHeaders[key][0]);
-        }
+        // for (key in responseHeaders) {
+        //     res.set(key, responseHeaders[key][0]);
+        // }
+        console.log(responseHeaders);
         res.send(responseData);
     } catch {
-        res.send("");
+        res.status(404).send('Something broke!');
     }
 
 });
@@ -49,7 +49,7 @@ router.post('/*', async function (req, res, next) {
     try {
         headers["origin"] = new URL(url).origin;
         headers["host"] = new URL(url).host;
-        // headers['contentType'] = 'application/x-www-form-urlencoded';
+
         //TODO: contentType 별로 body 바꿀 것!
         const body = JSON.parse(JSON.stringify(req.body));
         const response = await fetch(url, {
@@ -61,12 +61,13 @@ router.post('/*', async function (req, res, next) {
 
         const responseHeaders = JSON.parse(JSON.stringify(response.headers.raw()));
         const responseData = await response.text();
-        for (key in responseHeaders) {
-            res.set(key, responseHeaders[key][0]);
-        }
+        // for (key in responseHeaders) {
+        //     res.set(key, responseHeaders[key].join(";"));
+        // }
+        console.log(responseHeaders);
         res.send(responseData);
     } catch (e) {
-        res.send("");
+        res.status(404).send('Something broke!');
     }
 });
 module.exports = router;
