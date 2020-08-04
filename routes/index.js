0073d6dcff0d06be88e8c06e58395e9d053f2151
zaeval <zaeval@kookmin.ac.kr>
2020-08-05 00:45:34 +0900

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const fetch = require('node-fetch');
 const https = require('https');
+const FormData = require('form-data');
 
 const httpsAgent = new https.Agent({
     rejectUnauthorized: false,
@@ -15,6 +16,7 @@ router.get('/*', async function (req, res, next) {
     try {
         headers["origin"] = new URL(url).origin;
         headers["host"] = new URL(url).host;
+        headers["user-agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36";
 
         const response = await fetch(url, {headers: headers, agent: httpsAgent, method: 'GET'});
         const responseHeaders = JSON.parse(JSON.stringify(response.headers.raw()));
@@ -24,10 +26,17 @@ router.get('/*', async function (req, res, next) {
         }
         res.send(responseData);
     } catch {
+        console.log(e.message);
         res.send("");
     }
 
 });
+
+function getFormData(object) {
+    const formData = new FormData();
+    Object.keys(object).forEach(key => formData.append(key, object[key]));
+    return formData;
+}
 
 router.post('/*', async function (req, res, next) {
     const url = req.path.slice(1);
@@ -35,15 +44,25 @@ router.post('/*', async function (req, res, next) {
     try {
         headers["origin"] = new URL(url).origin;
         headers["host"] = new URL(url).host;
+        // headers['contentType'] = 'application/x-www-form-urlencoded';
 
-        const response = await fetch(url, {headers: headers, agent: httpsAgent, method: 'POST', body: JSON.parse(JSON.stringify(req.body))});
+        const body = JSON.parse(JSON.stringify(req.body));
+        console.log(        getFormData(body));
+        const response = await fetch(url, {
+            headers: headers,
+            agent: httpsAgent,
+            method: 'POST',
+            body: getFormData(body)
+        });
+
         const responseHeaders = JSON.parse(JSON.stringify(response.headers.raw()));
         const responseData = await response.text();
         for (key in responseHeaders) {
             res.set(key, responseHeaders[key][0]);
         }
+        console.log(responseData);
         res.send(responseData);
-    } catch(e) {
+    } catch (e) {
         console.log(e.message);
         res.send("");
     }
