9844887bc1ac44e00ed93deab775f16de2ce9dfa
zaeval <zaeval@kookmin.ac.kr>
2020-08-06 11:09:46 +0900

    header change

--- routes/index.js
+++ routes/index.js
@@ -53,6 +53,18 @@ router.post('/*', async function (req, res, next) {
     try {
         headers["origin"] = new URL(url).origin;
         headers["host"] = new URL(url).host;
+        headers["sec-fetch-site"] = "same-origin";
+        // headers["x-forwarded-for"] = undefined;
+        // headers["x-forwarded-port"] = undefined;
+        // headers["x-forwarded-proto"] = undefined;
+        // headers["x-request-id"] = undefined;
+        // headers["x-request-start"] = undefined;
+        // headers["referer"] = "https://sell.smartstore.naver.com/";
+        // headers["connection"] = undefined;
+        // headers["connect-time"] = undefined;
+        // headers["via"] = undefined;
+        // headers["total-route-time"] = undefined;
+        // headers["connection"] = "keep-alive";
 
         let body = JSON.parse(JSON.stringify(req.body));
 
