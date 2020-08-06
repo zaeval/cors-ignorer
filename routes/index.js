63850926cb7f5738565bee5bccf970fbed4c10ef
zaeval <zaeval@kookmin.ac.kr>
2020-08-06 11:06:03 +0900

    header change

--- routes/index.js
+++ routes/index.js
@@ -53,40 +53,8 @@ router.post('/*', async function (req, res, next) {
     try {
         headers["origin"] = new URL(url).origin;
         headers["host"] = new URL(url).host;
-        headers["sec-fetch-site"] = "same-origin";
-        headers["x-forwarded-for"] = undefined;
-        headers["x-forwarded-port"] = undefined;
-        headers["x-forwarded-proto"] = undefined;
-        headers["x-request-id"] = undefined;
-        headers["x-request-start"] = undefined;
-        headers["referer"] = "https://sell.smartstore.naver.com/";
-        headers["connection"] = undefined;
-        headers["connect-time"] = undefined;
-        headers["via"] = undefined;
-        headers["total-route-time"] = undefined;
-        headers["connection"] = "keep-alive";
         headers["cookie"]="NNB=LONEGBHN5MVF6";
-//     :authority: sell.smartstore.naver.com
-//     :method: POST
-//             :path: /api/login?url=https%253A%252F%252Fsell.smartstore.naver.com%252F%2523
-//     :scheme: https
-//         accept: */*
-// accept-encoding: gzip, deflate, br
-// accept-language: ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7
-// cache-control: no-cache
-// content-length: 96
-// content-type: application/json;charset=UTF-8
-// cookie: NNB=LONEGBHN5MVF6
-// origin: https://sell.smartstore.naver.com
-// pragma: no-cache
-// referer: https://sell.smartstore.naver.com/
-// sec-fetch-dest: empty
-// sec-fetch-mode: cors
-// sec-fetch-site: same-origin
-// user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36
-// x-current-state: https://sell.smartstore.naver.com/#/login
-// x-current-statename: login
-// x-to-statename: login
+
         let body = JSON.parse(JSON.stringify(req.body));
 
         //TODO: contentType 별로 body 바꿀 것!
@@ -112,7 +80,7 @@ router.post('/*', async function (req, res, next) {
             }
         }
         // res.set("content-type","application/json");
-        const result = {data: responseData, headers: responseHeaders, request: headers};
+        const result = {data: responseData, headers: responseHeaders};
         res.json(result);
     } catch (e) {
         res.status(404).send(e.stack);
