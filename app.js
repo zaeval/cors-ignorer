982f89c56bf5858ef94effdd165e7a83263d3249
zaeval <zaeval@kookmin.ac.kr>
2024-03-28 17:51:28 +0900

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.set('port',8080);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
